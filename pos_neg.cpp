1614d8c2fdf735b8ac6abd2ba6e12fb6f10f800e
Yashveer Singh Makhloga <119954921+Yashveer325007@users.noreply.github.com>
2024-05-04 23:19:20 +0530

    Create pos_neg.cpp

---
+++ pos_neg.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin>>a;
+    if(a>0){
+        cout<<"a is a positive number"<<endl;
+    }else if(a==0){
+        cout<<"a is equal to zero"<<endl;
+    }else{
+        cout<<"a is a negative number";
+    }
+
+}
